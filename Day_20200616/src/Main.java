e413a3c7284ffa43aeb1d41ad3832faba5c541bc
Danny320 <2261536364@qq.com>
2020-06-16 23:54:28 +0800

    1

---
+++ Day_20200616/src/Main.java
@@ -0,0 +1,22 @@
+/**
+ * Created with Intellij IDEA
+ *
+ * @program: Day_20200616
+ * @Description:
+ * @USer:LD
+ * @Date: 2020-06-16 16:47
+ * @time: 16:47
+ **/
+public class Main {
+    static boolean out(char c) {
+        System.out.println(c);
+        return true;
+    }
+    public static void main(String[] args) {
+        int i = 0;
+        for (out('A');out('B') && (i<2);out('c') ){
+            i++;
+        out('D');
+    }
+    }
+}
