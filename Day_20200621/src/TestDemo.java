e3c7a0287ea6a2aeb7d9eeb7a3f39fd35667f863
Danny320 <2261536364@qq.com>
2020-06-23 23:25:38 +0800

    1

--- Day_20200621/src/TestDemo.java
+++ Day_20200621/src/TestDemo.java
@@ -13,6 +13,97 @@ public class TestDemo {
 
 
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNext()) {
+            int N = sc.nextInt();
+
+        }
+    }
+
+
+
+    public static void main5(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNext()) {
+            int p = sc.nextInt();
+            if(p == 0) {
+              return;
+            }
+            int[] arr = new int[p];
+            for(int i = 0; i < arr.length; i++) {
+                arr[i] = sc.nextInt();
+            }
+            int t = sc.nextInt();
+            int count = 0;
+            for(int c : arr) {
+                if(c == t) {
+                    count++;
+                }
+            }
+            System.out.print(count);
+        }
+    }
+
+
+    public static void main4(String[] args) {
+       Scanner sc = new Scanner(System.in);
+       int groups = sc.nextInt();
+       while (groups-- > 0) {
+           int n = sc.nextInt();
+           int k = sc.nextInt();
+           int[] res = new int[2*n];
+           for (int i = 0; i < 2*n; i++) {
+               int tmp = i+1;
+               for(int j = 0; j < k; j++){
+                   if(tmp <= n) {
+                       tmp = 2*tmp -1;
+                   }else {
+                       tmp = 2 * (tmp -n);
+                   }
+               }
+               res[tmp-1] = sc.nextInt();
+           }
+           if(res.length >0) {
+               System.out.print(res[0]);
+           }
+           for (int i = 0; i < 2*n; i++) {
+               System.out.print(" "+res[i]);
+           }
+           System.out.println();
+       }
+    }
+
+
+
+    public static double GiveRatio(String str) {
+        double ratio = 0;
+        double count = 0;
+        for(char ch : str.toCharArray()) {
+            if(ch == 'G' || ch == 'C') {
+                count++;
+            }
+        }
+        ratio = count/(double) str.length();
+        return ratio;
+    }
+    public static void main3(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String dna = sc.nextLine();
+        int usedSize = sc.nextInt();
+        String maxStr = "";
+        double maxRatio = 0;
+        for(int i = 0; i < dna.length()-usedSize+1; i++) {
+            String str = dna.substring(i,i+usedSize);
+            if(GiveRatio(str) > maxRatio) {
+                maxStr = str;
+                maxRatio = GiveRatio(str);
+            }
+        }
+        System.out.println(maxStr);
+    }
+
+
+    public static void main2(String[] args) {
         System.out.print(10 * 30);
 
         int a = 0;
@@ -21,32 +112,6 @@ public class TestDemo {
         }
         System.out.print(" " + a);
     }
-
-
-    public static void main2(String[] args){
-        Scanner sc = new Scanner(System.in);
-        String input = sc.next().trim();
-        int n = sc.nextInt();
-        sc.close();
-
-        int max = 0;
-        String result = null;
-        for(int i = 0; i < input.length()-5; i++){
-            //每次截取制定长度的字串进行遍历
-            String temp = input.substring(i, i+n);
-            int length = 0;
-            for(int j = 0; j < n; j++){
-                if(temp.charAt(j) == 'G' || temp.charAt(j) == 'C')
-                    length++;
-            }
-            if(max < length){
-                max = length;
-                result = temp;
-            }
-        }
-        System.out.println(result);
-    }
-
 
 
     public static void main1(String[] args) {
---
+++ Day_20200623/src/TestDemo.java
@@ -0,0 +1,69 @@
+import java.util.*;
+
+/**
+ * Created with Intellij IDEA
+ *
+ * @program: Day_20200623
+ * @Description:
+ * @USer:LD
+ * @Date: 2020-06-23 21:17
+ * @time: 21:17
+ **/
+public class TestDemo {
+    public int getValue(int[] gifts, int n) {
+        // write code here
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for(int i = 0; i < gifts.length; i++) {
+            if(!map.containsKey(gifts[i])) {
+                map.put(gifts[i],1);
+            }else {
+                int v = map.get(gifts[i]);
+                if(v+1 > n/2) {
+                    return gifts[i];
+                }
+                map.put(gifts[i],v+1);
+            }
+        }
+        return 0;
+    }
+
+
+
+    public boolean[] chkSubStr(String[] p, int n, String s) {
+        // write code here
+        boolean[] arr = new boolean[n];
+        for (int i = 0; i < n; i++) {
+            if (s.contains(p[i])) {
+                arr[i] = true;
+            }else {
+                arr[i] = false;
+            }
+        }
+        return arr;
+    }
+
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        HashMap<Integer,String> map = new HashMap<>();
+        List<Integer> list = new ArrayList<>();
+        while (sc.hasNext()) {
+            String[] s = new String[2];
+            for(int i = 0; i < s.length; i++) {
+                s[i] = sc.next();
+            }
+         map.put(Integer.getInteger(s[1]),s[0]);
+         list.add(Integer.getInteger(s[1]));
+        }
+        list.sort(new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                return o1-o2;
+            }
+        });
+        for (int v: list) {
+            System.out.println(map.get(v)+v);
+        }
+    }
+}
