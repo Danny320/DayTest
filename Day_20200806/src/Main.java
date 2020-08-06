b8afb2aba02c00af4f61d01d88649581d82088c1
Danny320 <2261536364@qq.com>
2020-08-06 22:17:58 +0800

    1

---
+++ Day_20200806/src/Main.java
@@ -0,0 +1,50 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+/**
+ * @author LD
+ * @date 2020/8/6 22:02
+ */
+public class Main {
+public int MoreThanhalfNum_Solution(int[] array) {
+    Arrays.sort(array);
+    int count = 0;
+    for(int i = 0; i<array.length; i++) {
+        if(array[i] == array[array.length/2]) {
+            count++;
+        }
+    }
+    if(count > array.length/2) {
+        return array[array.length/2];
+    }else {
+        return 0;
+    }
+}
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while(sc.hasNext()) {
+            int year = sc.nextInt();
+            int month = sc.nextInt();
+            int day = sc.nextInt();
+            int Day = outDay(year,month,day);
+            System.out.println(Day);
+        }
+    }
+    public static int outDay(int year, int month, int day) {
+        int[] Day = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        if(year <= 0 || month <=0 || month>12 || day<=0 || day>Day[month-1]) {
+            return -1;
+        }
+        if(year%4==0&&year%100!=0 || year%400==0) {
+            Day[1] = 29;
+        }
+        int sum = 0;
+        for(int i = 0; i < month-1; i++) {
+            sum+=Day[i];
+        }
+        sum+=day;
+        return sum;
+    }
+
+
+}
