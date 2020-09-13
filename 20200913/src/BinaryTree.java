cf20f167f406140b995d8f1c50504dcf480a4691
Danny320 <2261536364@qq.com>
2020-09-13 22:42:15 +0800

    1

---
+++ 20200913/src/BinaryTree.java
@@ -0,0 +1,17 @@
+/**
+ * @author LD
+ * @date 2020/9/13 15:52
+ */
+
+class Node {
+    public char val;
+    public Node left;
+    public Node right;
+    public Node(char val) {
+        this.val = val;
+    }
+}
+
+public class BinaryTree {
+
+}
